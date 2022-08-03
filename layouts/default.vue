8ce18eda3e23c8d2f0929e7aced85b9e96b7d3f2
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-08-04 00:25:27 +0900

    trivial fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="overflow-hidden">
     <div class="sticky top-0 z-10 flex flex-row items-center bg-bg px-1">
       <div class="shrink-0">
         <slot name="back"></slot>
